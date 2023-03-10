35055147f347c58429830322b930ec7a3eaca705
zxyIsStudent <46954827+zxyIsStudent@users.noreply.github.com>
2023-03-10 16:10:13 +0800

    ll

--- files_mananger.sql
+++ files_mananger.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 09/03/2023 16:52:51
+ Date: 10/03/2023 15:42:04
 */
 
 SET NAMES utf8mb4;
@@ -23,12 +23,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `files_inform`;
 CREATE TABLE `files_inform` (
   `file_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
   `file_path` varchar(255) DEFAULT NULL,
   `file_name` varchar(255) DEFAULT NULL,
   `file_md5` varchar(255) DEFAULT NULL,
   `cte_time` datetime DEFAULT NULL,
   PRIMARY KEY (`file_id`),
   KEY `index1` (`file_md5`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1457 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1470 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
